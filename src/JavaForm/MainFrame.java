3f0403f80a59127ee0e0f5ec90966b44fef14563
CapEugene <evgexa_2003@bk.ru>
2025-10-02 11:44:29 +0300

    Add BoxLayout to MainPanel

--- src/JavaForm/MainFrame.java
+++ src/JavaForm/MainFrame.java
@@ -6,10 +6,11 @@ import java.awt.*;
 public class MainFrame extends JFrame {
     public MainFrame() {
         setTitle("Регистрация/авторизация");
-        setSize(1280, 720);
+        setSize(720, 480);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         MainPanel regAuthPanel = new MainPanel();
+        regAuthPanel.setLayout(new BoxLayout(regAuthPanel, BoxLayout.Y_AXIS));
         getContentPane().add(regAuthPanel);
 
     }
